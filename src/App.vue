90edcb921ab9a0f7bda2ec0efb9aca7b7205b2e3
tyrannosaurus-lee <miaow106@gmail.com>
2021-11-08 03:23:01 +0900

    하단 아이콘 바로가기 레이아웃 정리

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  height:100%; text-align: center;
   color: #2c3e50;
 }
 
